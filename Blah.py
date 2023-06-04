418d844032f67c2012d83ddf5e17bb275e889390
PranjaliRaval <101783860+PranjaliRaval@users.noreply.github.com>
2023-06-05 01:08:53 +0530

    Add files via upload

---
+++ Blah.py
@@ -0,0 +1,32 @@
+print("Welcome to The HUNT")
+print("\n Please Select Your Choice From Following.")
+print("1.Go to right")
+print("2. Go to LEFT")
+print("3. Exit ")
+a = int(input("Enter Your Choice"))
+if a == 1:
+    print("You died Dead")
+elif a == 2:
+    print("you have two choices ")
+    print("1.swim")
+    print("2.wait")
+else:
+    print("Loser")
+a== int(input("Try your luck"))
+if a == 1:
+    print("shark ate you")
+elif a == 2:
+    print("there are three doors in front")
+    print("1. Red")
+    print("2.Yellow")
+    print("3.Blue")
+else:
+    print("you are just bad at this")
+a == int(input("Enter your door"))
+if a == 1:
+    print("burn in hell fire")
+elif a == 2:
+    print("it was all yellow \n YOU WON")
+elif a == 3:
+    print("you are being eaten by hungry beast")
+

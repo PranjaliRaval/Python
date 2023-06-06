1b15242944dfed35ce37af92ba70c2718484f2df
PranjaliRaval <101783860+PranjaliRaval@users.noreply.github.com>
2023-06-07 02:40:03 +0530

    Add files via upload

---
+++ BasicBandNameGenerator.py
@@ -0,0 +1,7 @@
+#EasyBandNameGenerator
+print("Welcome to Easy Band Name Generator")
+cityname = input("In which city did you grow up in?\n: ")
+print(cityname)
+petname = input("What is your Favorite Pet's Name?\n: ")
+print(petname)
+print("Your band name could be " + cityname + " " +petname)
